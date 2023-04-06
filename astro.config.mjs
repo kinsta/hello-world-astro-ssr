082e812c6ac58a0ae68e223f7e3e5bbe1727db21
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-04-06 11:44:11 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(
+		{
+			extendMarkdownConfig: false,
+			smartypants: true,
+			gfm: true,
+		}
+	), sitemap()],
+});
