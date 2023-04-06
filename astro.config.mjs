4f6b8955b3356e6f406108276679f2702c1795f0
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-04-06 12:27:50 +0200

    config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://hello-world-astro-ssr-oypwt.kinsta.app',
 	output: 'server',
 	adapter: node({
 		mode: "standalone"
